676c7df096429f55c418603d6c02527b81442b18
valeraju <julien.valera@reseau.eseo.fr>
2018-03-16 17:52:59 +0100

    commit modifications

--- gestionTaxi/src/fr/eseo/jee/bdd/GestionTable.java
+++ gestionTaxi/src/fr/eseo/jee/bdd/GestionTable.java
@@ -1,5 +1,33 @@
 package fr.eseo.jee.bdd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import fr.eseo.jee.modele.Taxi;
+
 public class GestionTable {
 
+	public ArrayList<Taxi> getTaxis() {
+		ArrayList<Taxi>  result = new ArrayList<Taxi>(); 
+		try {
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/Sujet4?&characterEncoding=UTF8&useSSL=true&user=root&password=password");
+			Statement stat = connect.createStatement();
+			stat.executeQuery("SELECT * FROM Taxi");
+			ResultSet rset = stat.getResultSet();
+			while(rset.next()) {
+				result.add(new Taxi(rset.getInt("idTaxi"),rset.getString("categorie"),rset.getString("tarifDeBase"),rset.getString("ville")));
+			}
+			rset.close();
+			stat.close();
+			connect.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 }
--- gestionTaxi/src/fr/eseo/jee/ws/GestionTaxi.java
+++ gestionTaxi/src/fr/eseo/jee/ws/GestionTaxi.java
@@ -1,5 +1,34 @@
 package fr.eseo.jee.ws;
 
+import java.util.ArrayList;
+
+import fr.eseo.jee.bdd.GestionTable;
+import fr.eseo.jee.modele.Taxi;
+
 public class GestionTaxi {
 	
+	public GestionTable gestionTable;
+	
+	public GestionTaxi() {
+		this.gestionTable=new GestionTable();
+	}
+	
+	public Taxi[] trouverTaxi(ArrayList<Taxi> taxis) {
+		
+		Taxi[] result = new Taxi[5];
+		return result;
+	}
+
+	public GestionTable getGestionTable() {
+		return gestionTable;
+	}
+
+	public void setGestionTable(GestionTable gestionTable) {
+		this.gestionTable = gestionTable;
+	}
+	
+	
+	
 }
+
+
