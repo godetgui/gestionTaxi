2fb5b95623371491f288d00f60da8b7262ded0e6
Guillaume Godet <guillaume.godet@reseau.eseo.fr>
2018-03-16 14:18:09 +0100

    Nouveau script SQL de creation des tables

---
+++ gestionTaxi/sql/gestionTaxi.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le :  ven. 16 mars 2018 à 13:11
+-- Version du serveur :  5.7.21
+-- Version de PHP :  5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+DROP TABLE Reservation;
+DROP TABLE Client;
+DROP TABLE Taxi;
+
+--
+-- Base de données :  `gestionTaxi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Client`
+--
+
+CREATE TABLE `Client` (
+  `idClient` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `adresse` varchar(200) NOT NULL,
+  `telephone` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Reservation`
+--
+
+CREATE TABLE `Reservation` (
+  `idReservation` int(11) NOT NULL,
+  `dateReservation` date DEFAULT NULL,
+  `heure` time DEFAULT NULL,
+  `destination` varchar(50) DEFAULT NULL,
+  `booleenPaiementEffectue` tinyint(1) DEFAULT NULL,
+  `idClient` int(11) DEFAULT NULL,
+  `idTaxi` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Taxi`
+--
+
+CREATE TABLE `Taxi` (
+  `idTaxi` int(11) NOT NULL,
+  `categorie` varchar(50) NOT NULL,
+  `tarifDeBase` decimal(10,0) NOT NULL,
+  `ville` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Client`
+--
+ALTER TABLE `Client`
+  ADD PRIMARY KEY (`idClient`);
+
+--
+-- Index pour la table `Reservation`
+--
+ALTER TABLE `Reservation`
+  ADD PRIMARY KEY (`idReservation`),
+  ADD KEY `idClient` (`idClient`),
+  ADD KEY `idTaxi` (`idTaxi`);
+
+--
+-- Index pour la table `Taxi`
+--
+ALTER TABLE `Taxi`
+  ADD PRIMARY KEY (`idTaxi`);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `Reservation`
+--
+ALTER TABLE `Reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`idClient`) REFERENCES `Client` (`idClient`),
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`idTaxi`) REFERENCES `Taxi` (`idTaxi`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- gestionTaxi/src/fr/eseo/jee/modele/ReservationTaxi.java
+++ gestionTaxi/src/fr/eseo/jee/modele/ReservationTaxi.java
@@ -1,5 +1,57 @@
 package fr.eseo.jee.modele;
 
+import java.util.Calendar;
+
 public class ReservationTaxi {
+	
+	int idReservation;
+	Calendar date;
+	Calendar heure;
+	String destination;
+	boolean paiementEffectue;
+	
+	public int getIdReservation() {
+		return idReservation;
+	}
+	public void setIdReservation(int idReservation) {
+		this.idReservation = idReservation;
+	}
+	public Calendar getDate() {
+		return date;
+	}
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+	public Calendar getHeure() {
+		return heure;
+	}
+	public void setHeure(Calendar heure) {
+		this.heure = heure;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public boolean isPaiementEffectue() {
+		return paiementEffectue;
+	}
+	public void setPaiementEffectue(boolean paiementEffectue) {
+		this.paiementEffectue = paiementEffectue;
+	}
+	
+	
+	public ReservationTaxi(int idReservation, Calendar date, Calendar heure, String destination,
+			boolean paiementEffectue) {
+		super();
+		this.idReservation = idReservation;
+		this.date = date;
+		this.heure = heure;
+		this.destination = destination;
+		this.paiementEffectue = paiementEffectue;
+	}
+	
+	
 
 }
