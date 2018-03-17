c646d6741f10351f75ca94f84d83ade7752755f4
valeraju <julien.valera@reseau.eseo.fr>
2018-03-17 15:35:59 +0100

    modification trouverTaxi() method in GestionTaxi.java

--- gestionTaxi/src/fr/eseo/jee/bdd/GestionTable.java
+++ gestionTaxi/src/fr/eseo/jee/bdd/GestionTable.java
@@ -11,24 +11,27 @@ import fr.eseo.jee.modele.Taxi;
 
 public class GestionTable {
 	
-	public static void main(String[] args) throws SQLException {
-		DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-		Connection db = DriverManager.getConnection("jdbc:mysql://localhost/gestionTaxi?user=root&password=");
-		Statement s1 = db.createStatement();
-		s1.executeUpdate("Insert into Client values(1,'godet','guillaume','XX','0102030400')");
-	}
+//	public static void main(String[] args) throws SQLException {
+//		DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+//		Connection db = DriverManager.getConnection("jdbc:mysql://localhost/gestionTaxi?user=root&password=");
+//		Statement s1 = db.createStatement();
+//		s1.executeUpdate("Insert into Client values(1,'godet','guillaume','XX','0102030400')");
+//	}
 	
-	public ArrayList<Taxi> getTaxis() {
-		ArrayList<Taxi>  result = new ArrayList<Taxi>(); 
+	
+	public static ArrayList<String> getVille() {
+		ArrayList<String> result=new ArrayList<String>();
 		try {
 			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/Sujet4?&characterEncoding=UTF8&useSSL=true&user=root&password=password");
 			Statement stat = connect.createStatement();
-			stat.executeQuery("SELECT * FROM Taxi");
+//			stat.executeQuery("SELECT COUNT(DISTINCT Taxi.ville) AS nombre FROM TAXI");
+			stat.executeQuery("SELECT Taxi.ville FROM Taxi GROUP BY Taxi.ville");
 			ResultSet rset = stat.getResultSet();
 			while(rset.next()) {
-				result.add(new Taxi(rset.getInt("idTaxi"),rset.getString("categorie"),rset.getString("tarifDeBase"),rset.getString("ville")));
+				result.add(rset.getString("ville"));
 			}
+			System.out.println("RESULT="+result);
 			rset.close();
 			stat.close();
 			connect.close();
--- gestionTaxi/src/fr/eseo/jee/ws/GestionTaxi.java
+++ gestionTaxi/src/fr/eseo/jee/ws/GestionTaxi.java
@@ -1,32 +1,38 @@
 package fr.eseo.jee.ws;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
-import fr.eseo.jee.bdd.GestionTable;
 import fr.eseo.jee.modele.Taxi;
 
 public class GestionTaxi {
-	
-	public GestionTable gestionTable;
-	
-	public GestionTaxi() {
-		this.gestionTable=new GestionTable();
-	}
-	
-	public Taxi[] trouverTaxi(ArrayList<Taxi> taxis) {
 		
-		Taxi[] result = new Taxi[5];
+	public ArrayList<Taxi> trouverTaxi(String ville, String categorie) {
+		ArrayList<Taxi> result = new ArrayList<Taxi>();
+		String request = "SELECT * FROM Taxi WHERE NOT EXISTS (SELECT * FROM Reservation WHERE Taxi.idTaxi=Reservation.idTaxi) AND Taxi.ville="+"'"+ville+"'"+ "and Taxi.categorie="+"'"+categorie+"'";
+		try {
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/Sujet4?&characterEncoding=UTF8&useSSL=true&user=root&password=password");
+			Statement stat = connect.createStatement();
+			stat.executeQuery(request);
+			ResultSet rset = stat.getResultSet();
+			while(rset.next()) {
+				System.out.println("BONJOUR="+rset.getInt("idTaxi"));
+				result.add(new Taxi(rset.getInt("idTaxi"),rset.getString("categorie"),rset.getString("tarifDeBase"),rset.getString("ville")));
+			}
+			rset.close();
+			stat.close();
+			connect.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return result;
 	}
 
-	public GestionTable getGestionTable() {
-		return gestionTable;
-	}
-
-	public void setGestionTable(GestionTable gestionTable) {
-		this.gestionTable = gestionTable;
-	}
-	
 	
 	
 }
